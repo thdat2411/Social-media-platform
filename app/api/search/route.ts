760d745cc4691ce1fa4f8bc1e1a956387dbad75b
Phuong Dang <powoftech@gmail.com>
2024-12-09 23:46:24 +0700

    feat: Add ordering by creation date for user and post retrieval in search API

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -37,7 +37,11 @@ export async function GET(req: NextRequest) {
           include: {
             user: true,
           },
+          orderBy: {
+            created_at: "desc",
+          }
         });
+        
         const users = await prisma.user.findMany({
           where: {
             name: {
@@ -46,6 +50,7 @@ export async function GET(req: NextRequest) {
             },
           },
         });
+        
         const posts = await prisma.post.findMany({
           where: {
             content: {
